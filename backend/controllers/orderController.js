f7a89973041bfac3a872f36dd656c012b0451be1
Shreya Choudhary <139773235+shreyavchoudhary@users.noreply.github.com>
2026-05-11 16:56:13 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -86,15 +86,6 @@ const verifyOrder = async (req,res) => {
 
 }
 
-// const userOrders = async (req,res) =>{
-//     try{
-//         const orders = await orderModel.find({userId:req.body.userId}) ;
-//         res.json({success:true,data:orders})
-//     } catch (error) {
-//         console.log(error);
-//         res.json({success:false,message:"Error"})
-//     }
-// }
 const userOrders = async (req, res) => {
 
     try {
