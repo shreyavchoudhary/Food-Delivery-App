5635c4d608638ee2439193d73b8ffb3f46663b0f
shreya <shreyachoudhary2004we@gmail.com>
2026-05-11 16:48:35 +0530

    Fix admin API URLs (remove localhost)

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://food-del-backend-7x85.onrender.com" 
   return (
     <div>
     <ToastContainer />
