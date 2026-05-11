04f64027fd7e911cbc25f6ecf1391ea6761c8e8a
Shreya Choudhary <139773235+shreyavchoudhary@users.noreply.github.com>
2026-05-11 15:45:07 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -15,7 +15,7 @@ const FoodDisplay = ({category}) => {
     {food_list.map((item,index)=>{
       {console.log(category,item.category);}
       if(category==="All" || category===item.category){
-        return <Fooditem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+        return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
       }
     })}
     </div>
